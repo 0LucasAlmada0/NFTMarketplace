86023f0b42370fa681a1b613716b2f26eb042617
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-10-17 13:32:37 -0300

    Fix: Updating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar"
-import HeroSection from "./components/heroSection"
+import TrendingSection from "./components/TrendingSection"
+import HeroSection from "./components/HeroSection"
 
 function App() {
   return (
     <main className="bg-mainBackground">
       <Navbar/>
       <HeroSection/>
+      <TrendingSection/>
     </main>
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import ButtonAndIcon from "./shared/ButtonAndIcon";
-import NFTCard from '../components/NFTCard';
+import NFTCard from "../components/NFTCard";
 
 export default function HeroSection() {
   return (
@@ -12,7 +12,10 @@ export default function HeroSection() {
           NFT marketplace UI created with Anima for Figma. Collect, buy and sell
           art from more than 20k NFT artists.
         </p>
-        <ButtonAndIcon text={"Get Started"} icon={"src/assets/icons/RocketLaunch.png"}/>
+        <ButtonAndIcon
+          text={"Get Started"}
+          icon={"src/assets/icons/RocketLaunch.png"}
+        />
         <div className="flex my-6 gap-14">
           <div>
             <p className="font-bold text-2xl">240k+</p>
@@ -29,7 +32,12 @@ export default function HeroSection() {
         </div>
       </div>
       <div>
-        <NFTCard cardImagePath={"src/assets/images/ImagePlaceholder.png"} title={"Space Walking"} avatarName={"Animakid"} avatarIconPath={"src/assets/images/Avatar.png"}/>
+        <NFTCard
+          cardImagePath={"src/assets/images/ImagePlaceholder.png"}
+          title={"Space Walking"}
+          avatarName={"Animakid"}
+          avatarIconPath={"src/assets/images/Avatar.png"}
+        />
       </div>
     </section>
   );
