b4af83081a115d6da7e6fb81594e9db1289b9b09
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-10-18 13:29:09 -0300

    Feat: Creators Components

---
+++ src/components/CreatorsCard.jsx
@@ -0,0 +1,14 @@
+export default function CreatorsCard() {
+  return (
+    <div className="bg-[#3B3B3B] text-center rounded-3xl p-5">
+      <img
+        className="m-auto rounded-full"
+        src="https://picsum.photos/110"
+      ></img>
+      <h2 className="mt-6 mb-1 text-2xl font-bold">KeepItReal</h2>
+      <p className="text-[#858584]">
+        Total Sales: <b>34.53 ETH</b>
+      </p>
+    </div>
+  );
+}
---
+++ src/components/CreatorsSection.jsx
@@ -0,0 +1,37 @@
+import CreatorsCard from "./CreatorsCard";
+import ButtonAndIcon from "./shared/ButtonAndIcon";
+
+export default function CreatorSection() {
+  return (
+    <section className="py-[80px] mx-[10%]">
+      <div className="flex justify-between mb-20 items-end">
+        <div>
+          <h2 className="text-4xl font-bold">Top Creators</h2>
+          <p className="text-2xl">
+            Checkout Top Rated Creators on the NFT Marketplace
+          </p>
+        </div>
+        <div>
+          <ButtonAndIcon
+            icon={"src/assets/icons/RocketLaunch.png"}
+            text={"View Rankings"}
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-4 grid-rows-3 gap-8 text-center">
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+        <CreatorsCard />
+      </div>
+    </section>
+  );
+}
