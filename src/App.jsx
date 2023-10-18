c623063f4e5fd578366d45605149bf76da318f0d
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-10-18 13:25:50 -0300

    Fix: Adding new components into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar"
 import TrendingSection from "./components/TrendingSection"
 import HeroSection from "./components/HeroSection"
+import CreatorSection from "./components/CreatorsSection"
+import CategoriesSection from "./components/CategoriesSection"
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Navbar/>
       <HeroSection/>
       <TrendingSection/>
+      <CreatorSection/>
+      <CategoriesSection/>
     </main>
   )
 }
