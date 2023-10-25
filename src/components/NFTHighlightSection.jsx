884a93dec9aa928737a14fa7cf0b371f2fdd86a4
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-10-25 13:50:32 -0300

    Feat: 'NFTHighlightSection' Component

---
+++ src/components/NFTHighlightSection.jsx
@@ -0,0 +1,7 @@
+export default function NFTHighlightSection(){
+    return(
+        <section className="pb-10">
+            <img className="w-[100%]" src="https://picsum.photos/3000/650"></img>
+        </section>
+    )
+}
