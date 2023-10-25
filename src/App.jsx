87f7490fb1578b83706423ad2a8316b78c77e84c
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-10-25 13:48:54 -0300

    Fix: Inserting New Components in the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import Navbar from "./components/Navbar"
-import TrendingSection from "./components/TrendingSection"
-import HeroSection from "./components/HeroSection"
-import CreatorSection from "./components/CreatorsSection"
-import CategoriesSection from "./components/CategoriesSection"
+import Navbar from "./components/Navbar";
+import TrendingSection from "./components/TrendingSection";
+import HeroSection from "./components/HeroSection";
+import CreatorSection from "./components/CreatorsSection";
+import CategoriesSection from "./components/CategoriesSection";
+import DiscoverSection from "./components/DiscoverSection";
+import NFTHighlightSection from "./components/NFTHighlightSection";
+import HowItWorksSection from "./components/HowItWorksSection";
+import SubscribeWidgetSection from "./components/SubscribeWidgetSection";
 
 function App() {
   return (
     <main className="bg-mainBackground">
-      <Navbar/>
-      <HeroSection/>
-      <TrendingSection/>
-      <CreatorSection/>
-      <CategoriesSection/>
+      <Navbar />
+      <HeroSection />
+      <TrendingSection />
+      <CreatorSection />
+      <CategoriesSection />
+      <DiscoverSection />
+      <NFTHighlightSection/>
+      <HowItWorksSection/>
+      <SubscribeWidgetSection/>
     </main>
-  )
+  );
 }
-export default App
+export default App;
