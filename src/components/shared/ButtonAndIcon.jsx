320187f10310034b75deed7578cca3aa25f01c4f
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-10-26 13:18:56 -0300

    Fix: 'Link' tag from 'react-router-dom' lib added to Button component

--- src/components/shared/ButtonAndIcon.jsx
+++ src/components/shared/ButtonAndIcon.jsx
@@ -1,8 +1,12 @@
-export default function ButtonAndIcon({icon, text}) {
+import { Link } from "react-router-dom"
+
+export default function ButtonAndIcon({icon, text, link}) {
     return (
-        <button className="focus:outline-none flex bg-[#A259FF] text-white px-8 py-3 rounded-full gap-4 items-center">
-            <img src={icon}></img>
-            {text}
-        </button>
+        <Link to={link}>
+            <button className="focus:outline-none flex bg-[#A259FF] text-white px-8 py-3 rounded-full gap-4 items-center">
+                <img src={icon}></img>
+                {text}
+            </button>
+        </Link>
     )
 }
