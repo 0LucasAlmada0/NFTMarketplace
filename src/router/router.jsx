0686baed3bf155b44b0b3b66f6fd6eb8143fa7c7
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-11-02 13:24:12 -0300

    Fix: new pages routes added

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,22 +3,26 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "../layout/Layout";
 import HomePage from "../pages/HomePage";
 import CreateAccountPage from "../pages/CreateAccountPage";
+import ArtistPage from "../pages/ArtistPage";
 
 export const router = createBrowserRouter([
   {
-    path:'/',
-    element: <Layout/>,
-    children:[
+    path: "/",
+    element: <Layout />,
+    children: [
       {
-        path:'/',
-        element: <HomePage/>
+        path: "/",
+        element: <HomePage />,
       },
       {
-        path:'/createAccount',
-        element: <CreateAccountPage/>
+        path: "/createAccount",
+        element: <CreateAccountPage />,
       },
-    ]
+      {
+        // path: "/artist/:user",
+        path: "/artist",
+        element: <ArtistPage />,
+      },
+    ],
   },
-  
-])
-
+]);
