50388cbd8f63c961ccbd2780bd431eac4d58ddbe
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-11-29 23:11:23 -0300

    Fix: Adding new routes

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,11 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "../layout/Layout";
 import HomePage from "../pages/HomePage";
 import CreateAccountPage from "../pages/CreateAccountPage";
-<<<<<<< Updated upstream
 import ArtistPage from "../pages/ArtistPage";
-=======
+import MarketplacePage from "../pages/MarketplacePage";
 import ConnectWalletPage from "../pages/ConnectWalletPage";
->>>>>>> Stashed changes
 
 export const router = createBrowserRouter([
   {
@@ -22,14 +20,17 @@ export const router = createBrowserRouter([
         element: <CreateAccountPage />,
       },
       {
-<<<<<<< Updated upstream
         // path: "/artist/:user",
         path: "/artist",
         element: <ArtistPage />,
-=======
+      },
+      {
         path: "/connectWallet",
         element: <ConnectWalletPage />,
->>>>>>> Stashed changes
+      },
+      {
+        path: "/marketplace",
+        element: <MarketplacePage />,
       },
     ],
   },
