0bc03157ffb08fb33019e5f6aa9a97df94785f40
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-12-04 14:54:35 -0300

    Feat: 'UserContext' added to get which user is logged

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./router/router";
 import { RouterProvider } from "react-router-dom";
+import { app } from "./services/firebaseConfig";
+
+import { UserProvider } from "./context/UserContext";
+
+// app()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );
