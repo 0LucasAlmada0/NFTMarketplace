1ba1dd487db13e32e7eba90b82ad6968f1a66eba
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2023-12-04 14:56:32 -0300

    Fix: Show the current user on the 'Navbar'

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,12 @@
 import ButtonAndIcon from "../components/shared/ButtonAndIcon";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+import { UserContext } from "../context/UserContext";
 
 export default function Navbar() {
+  const { user } = useContext(UserContext);
+
   return (
     <nav className="flex py-6 px-14 items-center text-white font-bold">
       <Link className="flex flex-1 gap-4 items-center cursor-pointer">
@@ -10,16 +15,28 @@ export default function Navbar() {
       </Link>
       <div className="flex justify-end">
         <ul className="flex gap-12 items-center">
-          <li className="font-semibold">Marketplace</li>
-          <li className="font-semibold">Rankings</li>
-          <Link to={'/connectWallet'}>
-            <li className="font-semibold">Connect a wallet</li>
+          <Link to={"/marketplace"}>
+            <li className="font-semibold cursor-pointer hover:font-bold scale-120">
+              Marketplace
+            </li>
           </Link>
-          <ButtonAndIcon
-            icon={"/src/assets/icons/User.png"}
-            text={"Sign Up"}
-            link={"/createAccount"}
-          />
+          <li className="font-semibold cursor-pointer hover:font-bold scale-120">
+            Rankings
+          </li>
+          <Link to={"/connectWallet"}>
+            <li className="font-semibold cursor-pointer hover:font-bold scale-120">
+              Connect a wallet
+            </li>
+          </Link>
+          {user.email ? (
+            <h2 className="text-purple-400 text-xl">{user.email}</h2>
+          ) : (
+            <ButtonAndIcon
+              icon={"/src/assets/icons/User.png"}
+              text={"Sign Up"}
+              link={"/createAccount"}
+            />
+          )}
         </ul>
       </div>
     </nav>
